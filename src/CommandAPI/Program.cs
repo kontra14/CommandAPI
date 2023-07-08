b97532b0a78251ac300864846a8aa41636ce5ad9
Gerardo González <ggonza@vera.com.uy>
2023-07-08 20:49:53 -0300

    Commit inicial

---
+++ src/CommandAPI/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+ConfigureServices(builder.Services);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseRouting();
+
+app.MapGet("/", () => "Hello World!");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
+
+void ConfigureServices(IServiceCollection services)
+{
+    //SECTION 1. Add code below
+    services.AddControllers();
+}
