3b34461423ad8acd7e9e3d2becf95d5f03bf1e12
Gerardo González <ggonza@vera.com.uy>
2023-07-09 19:26:56 -0300

    Agrega conexión a la BD e implemtna 2 métodos con el repositorio real

--- src/CommandAPI/Program.cs
+++ src/CommandAPI/Program.cs
@@ -1,11 +1,18 @@
 using CommandAPI.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-ConfigureServices(builder.Services);
+//var configuration = builder.Configuration;
+
+//ConfigureServices(builder.Services, builder.Configuration);
 
 // Add services to the container.
 builder.Services.AddControllers();
+//builder.Services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+builder.Services.AddScoped<ICommandAPIRepo, SqlCommandAPIRepo>();
+builder.Services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
+    (builder.Configuration.GetConnectionString("PostgreSqlConnection")));
 
 var app = builder.Build();
 
@@ -18,9 +25,10 @@ app.MapControllerRoute(
 
 app.Run();
 
-void ConfigureServices(IServiceCollection services)
-{
-    //SECTION 1. Add code below
-    services.AddControllers();
-    services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
-}
+// void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+// {
+//     services.AddControllers();
+//     services.AddScoped<ICommandAPIRepo, MockCommandAPIRepo>();
+//     services.AddDbContext<CommandContext>(opt => opt.UseNpgsql
+//         (configuration.GetConnectionString("PostgreSqlConnection")));
+// }
