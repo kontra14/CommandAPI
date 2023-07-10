35081beb498fff328ab8accd1e90012a692b0324
Gerardo González <ggonza@vera.com.uy>
2023-07-10 10:11:29 -0300

    Primer xUnit test corriendo (CommandsController.GetAllCommands)

---
+++ test/CommandAPI.Tests/CommandTests.cs
@@ -0,0 +1,63 @@
+using System;
+using Xunit;
+using CommandAPI.Models;
+
+namespace CommandAPI.Tests
+{
+    public class CommandTests : IDisposable
+    {
+        Command testCommand;
+
+        public CommandTests()
+        {
+            testCommand = new Command
+            {
+                HowTo = "Do something",
+                Platform = "Some platform",
+                CommandLine = "Some commandline"
+            };
+        }
+        public void Dispose()
+        {
+            testCommand = null;
+        }
+
+        [Fact]
+        public void CanChangeHowTo()
+        {
+            //Arrange
+
+            //Act
+            testCommand.HowTo = "Execute Unit Tests";
+
+            //Assert
+            Assert.Equal("Execute Unit Tests", testCommand.HowTo);
+        }
+
+
+        [Fact]
+        public void CanChangePlatform()
+        {
+            //Arrange
+
+            //Act
+            testCommand.Platform = ".NET";
+
+            //Assert
+            Assert.Equal(".NET", testCommand.Platform);
+        }
+
+
+        [Fact]
+        public void CanChangeCommandLine()
+        {
+            //Arrange
+
+            //Act
+            testCommand.CommandLine = "dotnet test";
+
+            //Assert
+            Assert.Equal("dotnet test", testCommand.CommandLine);
+        }
+    }
+}
